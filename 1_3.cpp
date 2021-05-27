c47006400ae0672e1871844df620a107f2a783a6
youhuangla <1063615427@qq.com>
2021-05-27 08:51:33 +0800

    1_1 to 1_4

---
+++ 1_3.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 1_2.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sat 22 May 2021 11:10:05 PM CST
+ ************************************************************************/
+
+#include <iostream>
+
+int main() {
+    int x, y;
+    std::cin >> x >> y;
+    std::cout << x * y << std::endl;
+    return 0;
+}
