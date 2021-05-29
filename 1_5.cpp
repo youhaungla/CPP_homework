5c509f88b0cf355bb2fd22219840c608c53f055d
youhuangla <1063615427@qq.com>
2021-05-29 20:36:30 +0800

    1_5

---
+++ 1_5.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 1_5.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sat 29 May 2021 08:29:46 PM CST
+ ************************************************************************/
+
+#include <iostream>
+
+int main() {
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The product of " << v1 << " multiply " << v2 << " is: " << v1 * v2 << std::endl;
+    return 0;
+}
